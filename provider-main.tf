8e337d1c11559ce949f5e08d890d7ba376ec2493
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2020-05-13 17:11:50 +0200

    Initial commit

---
+++ provider-main.tf
@@ -0,0 +1,15 @@
+###########################
+## Azure Provider - Main ##
+###########################
+
+# Define Terraform provider
+terraform {
+  required_version = ">= 0.12"
+}
+
+# Configure the Azure provider
+provider "azurerm" { 
+  version = ">= 2.0.0"
+  features {}  
+  environment     = "public"
+}
